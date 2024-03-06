4c03a1f7ae89638a8b08369385a84890366e13d3
Elliot Thomas <eth0net@pm.me>
2024-03-06 15:23:39 +0000

    parse files in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::error::Error;
 use std::path::PathBuf;
 
 use clap::Parser;
+
+use cyborg::comic::Comic;
 
 #[derive(Debug, Parser)]
 #[command(version, author, about)]
@@ -24,10 +27,44 @@ struct Args {
     recursive: bool,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
-    println!("{:?}", args);
+    for path in args.targets {
+        process_path(path, args.recursive)?;
+    }
 
-    println!("Hello, comics!");
+    Ok(())
+}
+
+fn process_path(path: PathBuf, recursive: bool) -> Result<(), Box<dyn Error>> {
+    if path.is_dir() {
+        process_dir(path, recursive)?;
+    } else if path.is_file() {
+        process_file(path)?;
+    } else {
+        println!("{} is not a file or directory", path.display());
+    }
+
+    Ok(())
+}
+
+fn process_dir(dir: PathBuf, recursive: bool) -> Result<(), Box<dyn Error>> {
+    for entry in dir.read_dir()? {
+        let entry = entry?;
+        let path = entry.path();
+        process_path(path, recursive)?;
+    }
+
+    Ok(())
+}
+
+fn process_file(file: PathBuf) -> Result<(), Box<dyn Error>> {
+    let name = file.file_name().ok_or("No file name")?;
+
+    let new_name: Comic = name.to_str().unwrap().parse()?;
+
+    println!("{:?} => {}", name, new_name);
+
+    Ok(())
 }
